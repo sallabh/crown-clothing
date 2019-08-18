49815f917efb3de78aec0cd13f1e0a1c314dec71
sallabh <sallabhk@gmail.com>
2019-08-18 13:52:23 +0000

    integrated firebase in to application , created signin page with signin component and sign in with google

--- src/App.js
+++ src/App.js
@@ -3,21 +3,47 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import './App.css';
-import Header from './component/header/header.component.jsx'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './component/header/header.component.jsx';
+import {auth} from './firebase/firebase.utils';
 
 
 
-function App() {
-  return (
+
+class App extends React.Component {
+	constructor(){
+		super();
+		
+		this.state = {
+			currentUser : null
+		}
+	}
+	
+	unsubscribeFromAuth = null;
+	
+	componentDidMount(){
+		this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
+			this.setState({ currentUser : user});
+			console.log(user);
+		})
+	}
+	componentWillUnmount(){
+		this.unsubscribeFromAuth();
+	}
+	render() {
+		return (
     <div>
-		  <Header />
+		  <Header currentUser={this.state.currentUser}/>
 		  <Switch>
 			  	<Route exact path="/" component={HomePage}/>
 		  		<Route exact path="/shop" component={ShopPage}/>
+			  <Route exact path="/signin" component={SignInAndSignUpPage}/>
+			  
 		  </Switch>
 		  
     </div>
-  );
+	);
+	}
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,43 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCuVtwvZm9B1cbyBsQa39yWw0g-mxXFakU",
+    authDomain: "crown-db-3deba.firebaseapp.com",
+    databaseURL: "https://crown-db-3deba.firebaseio.com",
+    projectId: "crown-db-3deba",
+    storageBucket: "",
+    messagingSenderId: "832130415442",
+    appId: "1:832130415442:web:e49c102962a82aa6"
+  };
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider).then(function(result){
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+	console.log(token);
+   var user = result.user;
+	console.log(user);
+	}).catch(function(error) {
+	// Handle Errors here.
+  var errorCode = error.code;
+	console.log(errorCode);
+  var errorMessage = error.message;
+	// The email of the user's account used.
+	console.log(errorMessage);
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+	console.log(email);
+  var credential = error.credential;
+	console.log(credential);
+  // ...
+});
+
+export default firebase;
